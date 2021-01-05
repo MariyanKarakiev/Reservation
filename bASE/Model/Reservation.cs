b0a9975b6d3b73a4f56a0730c25d6ddeff53a502
MariyanKarakiev <77015673+MariyanKarakiev@users.noreply.github.com>
2021-01-05 18:48:56 +0200

    Add files via upload

---
+++ bASE/Model/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KurProject.Model
+{
+    class Reservation
+    {
+        public string NameofReservation { get; set; }
+        public string Age { get; set; }
+        public int RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public bool Smoke { get; set; }
+        public double CalculatedPrice { get; set; }
+        public DateTime PeriodOfReservation { get; set; }
+
+
+    }
+}
